21f166fbfa770f2937af90bf6269516948f8d613
madyt <madyt@novek.io>
2022-07-06 22:03:56 +0300

    mobile view navigator

--- src/components/header/mobile-drawer.js
+++ src/components/header/mobile-drawer.js
@@ -1,33 +1,34 @@
-import React, { useContext } from 'react';
-import { Box } from 'theme-ui';
-import { Scrollbars } from 'react-custom-scrollbars';
-import Drawer from 'components/drawer';
-import { DrawerContext } from '../../contexts/drawer/drawer.context';
-import { IoMdClose, IoMdMenu } from 'react-icons/io';
-import { Link } from 'react-scroll';
+import React, { useContext } from "react";
+import { Box } from "theme-ui";
+import { Scrollbars } from "react-custom-scrollbars";
+import Drawer from "components/drawer";
+import { DrawerContext } from "../../contexts/drawer/drawer.context";
+import { IoMdClose, IoMdMenu } from "react-icons/io";
+// import { Link } from "react-scroll";
+import Link from "next/link";
 import {
   FaFacebookF,
   FaTwitter,
   FaGithubAlt,
   FaDribbble,
-} from 'react-icons/fa';
-import menuItems from './header.data';
+} from "react-icons/fa";
+import menuItems from "./header.data";
 
 const social = [
   {
-    path: 'https://www.facebook.com',
+    path: "https://www.facebook.com",
     icon: <FaFacebookF />,
   },
   {
-    path: 'https://twitter.com/',
+    path: "https://twitter.com/",
     icon: <FaTwitter />,
   },
   {
-    path: 'https://github.com/',
+    path: "https://github.com/",
     icon: <FaGithubAlt />,
   },
   {
-    path: 'https://dribbble.com/',
+    path: "https://dribbble.com/",
     icon: <FaDribbble />,
   },
 ];
@@ -38,7 +39,7 @@ const MobileDrawer = () => {
   // Toggle drawer
   const toggleHandler = React.useCallback(() => {
     dispatch({
-      type: 'TOGGLE',
+      type: "TOGGLE",
     });
   }, [dispatch]);
 
@@ -60,15 +61,7 @@ const MobileDrawer = () => {
         <Box sx={styles.content}>
           <Box sx={styles.menu}>
             {menuItems.map(({ path, label }, i) => (
-              <Link
-                activeClass="active"
-                to={path}
-                spy={true}
-                smooth={true}
-                offset={-70}
-                duration={500}
-                key={i}
-              >
+              <Link href={path} key={i} sx={styles.mobileLink}>
                 {label}
               </Link>
             ))}
@@ -93,113 +86,113 @@ const MobileDrawer = () => {
 
 const styles = {
   handler: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    flexShrink: '0',
-    width: '26px',
-    cursor: 'pointer',
-    '@media screen and (min-width: 1024px)': {
-      display: 'none',
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    flexShrink: "0",
+    width: "26px",
+    cursor: "pointer",
+    "@media screen and (min-width: 1024px)": {
+      display: "none",
     },
   },
 
   drawer: {
-    width: '100%',
-    height: '100%',
-    backgroundColor: 'dark',
+    width: "100%",
+    height: "100%",
+    backgroundColor: "dark",
   },
 
   close: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    position: 'absolute',
-    top: '25px',
-    right: '30px',
-    zIndex: '1',
-    cursor: 'pointer',
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    position: "absolute",
+    top: "25px",
+    right: "30px",
+    zIndex: "1",
+    cursor: "pointer",
   },
 
   content: {
-    width: '100%',
-    height: '100%',
-    display: 'flex',
-    flexDirection: 'column',
-    pt: '100px',
-    pb: '40px',
-    px: '30px',
+    width: "100%",
+    height: "100%",
+    display: "flex",
+    flexDirection: "column",
+    pt: "100px",
+    pb: "40px",
+    px: "30px",
   },
 
   menu: {
-    width: '100%',
-    display: 'flex',
-    flexDirection: 'column',
+    width: "100%",
+    display: "flex",
+    flexDirection: "column",
     a: {
-      fontSize: '16px',
-      fontWeight: '500',
-      color: 'text_white',
-      py: '15px',
-      cursor: 'pointer',
-      borderBottom: '1px solid #e8e5e5',
-      transition: 'all 0.25s',
-      '&:hover': {
-        color: 'primary',
+      fontSize: "16px",
+      fontWeight: "500",
+      color: "text_white",
+      py: "15px",
+      cursor: "pointer",
+      borderBottom: "1px solid #e8e5e5",
+      transition: "all 0.25s",
+      "&:hover": {
+        color: "primary",
       },
-      '&.active': {
-        color: 'primary',
+      "&.active": {
+        color: "primary",
       },
     },
   },
 
   menuFooter: {
-    width: '100%',
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    mt: 'auto',
+    width: "100%",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    mt: "auto",
   },
 
   social: {
-    width: '100%',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
+    width: "100%",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
 
     icon: {
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center',
-      color: 'text',
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      color: "text",
       fontSize: 1,
       mr: 4,
-      transition: 'all 0.25s',
-      cursor: 'pointer',
+      transition: "all 0.25s",
+      cursor: "pointer",
       svg: {
-        fill: 'text',
-        transition: 'all 0.25s',
+        fill: "text",
+        transition: "all 0.25s",
       },
-      ':last-child': {
-        mr: '0',
+      ":last-child": {
+        mr: "0",
       },
-      '&:hover svg': {
-        fill: 'primary',
+      "&:hover svg": {
+        fill: "primary",
       },
     },
   },
 
   button: {
-    color: 'white',
-    fontSize: '14px',
-    fw: '700',
-    height: '45px',
-    borderRadius: '5px',
-    cursor: 'pointer',
-    width: '100%',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    py: '0',
+    color: "white",
+    fontSize: "14px",
+    fw: "700",
+    height: "45px",
+    borderRadius: "5px",
+    cursor: "pointer",
+    width: "100%",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    py: "0",
   },
 };
 
